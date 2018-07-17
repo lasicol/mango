eed11b7141ae8525452405bc6cba2bdbb6a327ec
Blazej Laszewski <lasicb@gmail.com>
2018-07-17 20:16:20 +0200

    second one

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import {app, BrowserWindow} from 'electron'
+import * as path from "path";
 
 let win = null
 
@@ -6,5 +7,7 @@ app.on('ready', () => {
     win = new BrowserWindow({
         darkTheme: true
     })
-    
+
+     // and load the index.html of the app.
+     win.loadFile(path.join(__dirname, "../index.html"))
 })
